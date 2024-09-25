8d0ddbfd405985efcab6259ff5408f2792bd2b70
Ah815 <83178033+Ah815@users.noreply.github.com>
2024-09-25 12:01:17 +0500

    new cv

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
               Message Me on WhatsApp
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1qEl9yE7PTEzduFdf19tHY3AL2q6ph49a/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1n5zq8tbj444zThZNOnmIt_wnz6CgSOLV/view?usp=drive_link"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-gray-900 hover:bg-slate-800 rounded-full px-5 py-2">
